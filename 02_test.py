18a3dcdb3dc502d19f281aa9f98a1fc6108c60f1
lacsec <lacsec@yahoo.co.jp>
2017-06-25 14:36:23 +0900

    Update 02_test.py

--- 02_test.py
+++ 02_test.py
@@ -1,1 +1,24 @@
+#from idaapi import *
+# ScreeenEA()
+print("ScreenEA is " + hex(ScreenEA()))
+
+#GetInputFileMD5()
+print("MD5 is " + hex(GetInputFileMD5()))
+
+#FirstSeg()
+print("FirstSegment is " + hex(FirstSeg()))
+#NextSeg()
+print("NextSegment is " + hex(NextSeg()))
+
+#SegByName(str)
+
+#SegEnd(long Address)
+
+#SegStart(long Address)
+
+#SegName(long Address)
+
+#Segments()
+
+# *** Function ***
 
